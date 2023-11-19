0d39220a2147f3aaa1900330c4e854559227ead9
Daniel Fewell <dpf711@ymail.com>
2023-11-18 21:56:42 -0500

    Refined UI and added logic to convert .doc and .docx to .pdf

--- ConverterTool/Form1.Designer.cs
+++ ConverterTool/Form1.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_select_file = new System.Windows.Forms.Button();
+            this.lb_chosen_file = new System.Windows.Forms.ListBox();
+            this.clb_conversion_options = new System.Windows.Forms.CheckedListBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.txtbox_step_2_instructions = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btn_select_file
+            // 
+            this.btn_select_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_select_file.Location = new System.Drawing.Point(57, 113);
+            this.btn_select_file.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_select_file.Name = "btn_select_file";
+            this.btn_select_file.Size = new System.Drawing.Size(205, 86);
+            this.btn_select_file.TabIndex = 0;
+            this.btn_select_file.Text = "Step 1: Choose File to Convert";
+            this.btn_select_file.UseVisualStyleBackColor = true;
+            this.btn_select_file.Click += new System.EventHandler(this.btn_select_file_Click);
+            // 
+            // lb_chosen_file
+            // 
+            this.lb_chosen_file.FormattingEnabled = true;
+            this.lb_chosen_file.Location = new System.Drawing.Point(57, 230);
+            this.lb_chosen_file.Name = "lb_chosen_file";
+            this.lb_chosen_file.Size = new System.Drawing.Size(205, 160);
+            this.lb_chosen_file.TabIndex = 1;
+            // 
+            // clb_conversion_options
+            // 
+            this.clb_conversion_options.FormattingEnabled = true;
+            this.clb_conversion_options.Location = new System.Drawing.Point(354, 215);
+            this.clb_conversion_options.Name = "clb_conversion_options";
+            this.clb_conversion_options.Size = new System.Drawing.Size(205, 94);
+            this.clb_conversion_options.TabIndex = 2;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_convert.Location = new System.Drawing.Point(319, 325);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(271, 86);
+            this.btn_convert.TabIndex = 3;
+            this.btn_convert.Text = "Perform Conversion";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // txtbox_step_2_instructions
+            // 
+            this.txtbox_step_2_instructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_step_2_instructions.Location = new System.Drawing.Point(319, 113);
+            this.txtbox_step_2_instructions.Multiline = true;
+            this.txtbox_step_2_instructions.Name = "txtbox_step_2_instructions";
+            this.txtbox_step_2_instructions.ReadOnly = true;
+            this.txtbox_step_2_instructions.Size = new System.Drawing.Size(271, 86);
+            this.txtbox_step_2_instructions.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1871, 1116);
+            this.ClientSize = new System.Drawing.Size(1062, 476);
+            this.Controls.Add(this.txtbox_step_2_instructions);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.clb_conversion_options);
+            this.Controls.Add(this.lb_chosen_file);
+            this.Controls.Add(this.btn_select_file);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_select_file;
+        private System.Windows.Forms.ListBox lb_chosen_file;
+        private System.Windows.Forms.CheckedListBox clb_conversion_options;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.TextBox txtbox_step_2_instructions;
     }
 }
 
--- ConverterTool/Form1.cs
+++ ConverterTool/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Spire.Doc; //Package relates to .doc & .docx to pdf 
+using Spire.Doc.Documents;//Package relates to .doc & .docx to pdf
+using System.Security; 
 
 namespace ConverterTool
 {
@@ -15,6 +19,119 @@ namespace ConverterTool
         public Form1()
         {
             InitializeComponent();
+
+            txtbox_step_2_instructions.Text = "Step 2: " + Environment.NewLine + Environment.NewLine + "Select a file conversion method." + Environment.NewLine + Environment.NewLine + "Then press the 'Perform Conversion' button.";
+        }
+
+        //Global variables
+        string selectedFilePath;
+
+        private void btn_select_file_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Configuration for openFileDialog
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePath = openFileDialog.FileName; // Assigns the selected file path to the global variable
+
+                    lb_chosen_file.Items.Clear(); // Clear previous entries
+                    lb_chosen_file.Items.Add(selectedFilePath); // Add the selected file path to the listbox
+
+                    string fileExtension = Path.GetExtension(selectedFilePath).ToLower();
+                    List<string> conversionOptionsList = GetConversionOptions(fileExtension);
+
+                    clb_conversion_options.Items.Clear(); // Clear existing items in the checked list box
+                    foreach (string option in conversionOptionsList)
+                    {
+                        clb_conversion_options.Items.Add(option); // Add options to the checked list box
+                    }
+                }
+            }
+        }
+
+        private List<string> GetConversionOptions(string fileExtension)
+        {
+            var options = new List<string>(); //Declare and initialize a new list of strings called options keyword | var allows compiler to infer variable type | new keyword allocates memory for for a new 'List<string> object
+
+            switch (fileExtension) //this switch statement is where the logic for populating the conversion options list in clb_conversion_options is contained
+            {
+                //add word to pdf conversion option to options list
+                case ".doc":
+                case ".docx":
+                    options.Add("Word to PDF");
+                    break;
+
+                //ADD MORE OPTIONS HERE
+            }
+            return options; //Function returns string list named options back to foreach statement in btn_select_file_Click that adds string option to the checked list box named clb_conversion_options
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            foreach (var item in clb_conversion_options.CheckedItems) //Iterate through the conversion_options list and store selected converson in var item
+            {
+                string conversionType = item.ToString(); //set string conversionType equal to the stringified var item
+
+                PerformConversion(conversionType); //call the PerformConvert function based on the data stored in string conversionType
+            }
+        }
+
+        private void PerformConversion(string conversionType)
+        {
+            switch (conversionType) //this switch statement checks the string stored in conversionType and calls the appropriate conversion function
+            {
+                //call word to pdf conversion
+                case "Word to PDF":
+                    ConvertWordToPdf();
+                    break;
+
+                //ADD MORE OPTIONS HERE
+            }
+        }
+
+        private string GetFilePathWithoutExtension(string filePath)
+        {
+            string directory = Path.GetDirectoryName(filePath);
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+            return Path.Combine(directory, fileNameWithoutExtension);
+        }
+
+        private void ConvertWordToPdf() 
+        {
+            //parts of this function rely on the Spire.Doc package
+
+            bool conversionSuccess = false;
+
+            try
+            {
+                string baseFilePath = GetFilePathWithoutExtension(selectedFilePath);
+                string pdfFilePath = baseFilePath + ".pdf";
+                Document doc = new Document(); //Create new instance of Spire.Doc document class called doc
+                doc.LoadFromFile(selectedFilePath); //Load word doc from file specified by wordFilePath, into the doc object
+
+                doc.SaveToFile(pdfFilePath, FileFormat.PDF); //Save the loaded document to a new file in PDF format
+
+                conversionSuccess = true;
+            }
+
+            catch (Exception ex)
+            {
+                conversionSuccess = false;
+            }
+            conversionStatusMessage(conversionSuccess);
+        }
+
+        private void conversionStatusMessage(bool operationStatus)
+        {
+            if (operationStatus)
+            {
+                MessageBox.Show("File Converted Successfully!");
+            } else
+            {
+                MessageBox.Show("File Conversion has Failed!");
+            }
         }
     }
 }
